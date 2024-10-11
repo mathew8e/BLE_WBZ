03f48f580229b371729c23d884184f305a0a4622
Mathew Bosak <123kumatan@gmail.com>
2024-10-11 15:19:18 +0200

    initial commit

---
+++ BLE_beggining.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=BLE_beggining.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/BLE_beggining.X.production.hex
